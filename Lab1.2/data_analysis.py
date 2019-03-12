9a001500573ec395dcbb0964e5b29f430059ffee
mpioner <ampioner@gmail.com>
2019-03-12 17:31:28 +0300

    pp

--- Lab1.2/data_analysis.py
+++ Lab1.2/data_analysis.py
@@ -1,13 +1,12 @@
 from matplotlib import pyplot
 from openpyxl import load_workbook
 import pprint
-
 import os
+
 #print (os.getcwd())
 wb = load_workbook('.\\Lab1.2\\data_analysis_lab.xlsx')
 
 sheet = wb['Data']
-#sheet['A'][1:]
 #pp = pprint.PrettyPrinter(indent=4)
 #pp.pprint(list(sheet))
 def getvalue(x): return x.value
